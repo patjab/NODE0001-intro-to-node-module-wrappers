1b5aa7de9ad8f881698f06dde6171e8524765e86
github username <github email address>
2018-07-08 15:08:38 -0400

    coded module wrapper arguments and function

---
+++ app.js
@@ -0,0 +1,11 @@
+/*  NODE.JS - Introduction to Module Wrapper Functions
+ *
+ *  Inspired by https://www.youtube.com/watch?v=TlB_eWDSMt4
+ */
+
+console.log("The __dirname is: ", __dirname)
+console.log("The __filename is: ", __filename)
+
+console.log("\nEXPORTS\n", exports)
+console.log("\nREQUIRE\n", require)
+console.log("\nMODULE\n", module)
